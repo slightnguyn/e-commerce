76fce4bf1ff08c4dc22894616580d089b3985b99
thoainguyn <thoainguyn@gmail.com>
2016-06-22 09:15:43 +0700

    Set handle error with each environment for app

--- app/init.php
+++ app/init.php
@@ -2,8 +2,15 @@
 
 set_include_path(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT'));
 date_default_timezone_set('Asia/Ho_Chi_Minh');
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+define('ENV', 'local');
+
+if (ENV == 'local') {
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+}
+else {
+	ini_set('display_errors', 0);
+}
 
 require 'app/helpers/function.php';
 require 'app/core/App.php';
