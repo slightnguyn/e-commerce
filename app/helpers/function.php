f31924b74fd66541748d16423544b8a486794a06
thoainguyn <thoainguyn@gmail.com>
2016-06-22 08:42:48 +0700

    Add helpers file

---
+++ app/helpers/function.php
@@ -0,0 +1,36 @@
+<?php
+
+function renderRating($star) {
+	for ($i = 1; $i <= $star; $i++) {
+		echo '<span class="text-info"><i class="fa fa-star"';
+		echo ($i == $star) ? 'data-toggle="tooltip" data-placement="top" title="' . titleForStar($i) . '"' : '';
+		echo '></i></span>';
+	}
+
+	for ($i = $star + 1; $i <= 5; $i++) {
+		echo '<span class="text-info"><i class="fa fa-star-o"></i></span>';
+	}
+}
+
+function titleForStar($num) {
+	switch ($num) {
+		case 1:
+			return 'Very bad';
+			break;
+		case 2:
+			return 'Bad';
+			break;
+		case 3:
+			return 'Normal';
+			break;
+		case 4:
+			return 'Good';
+			break;
+		case 5:
+			return 'Very good';
+			break;
+		default:
+			return 'Error!';
+			break;
+	}
+}
